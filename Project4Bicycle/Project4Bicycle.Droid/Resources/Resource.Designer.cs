6b5eb718045074f91748371641811c3fabc66454
Rick Zegelaar <Rick Zegelaar>
2016-06-29 10:51:49 +0200

    Added Syncfusion chart references.

--- Project4Bicycle/Project4Bicycle.Droid/Resources/Resource.Designer.cs
+++ Project4Bicycle/Project4Bicycle.Droid/Resources/Resource.Designer.cs
@@ -1531,6 +1531,18 @@ namespace Project4Bicycle.Droid
 			// aapt resource value: 0x7f04000d
 			public const int design_snackbar_out = 2130968589;
 			
+			// aapt resource value: 0x7f04000e
+			public const int fadein = 2130968590;
+			
+			// aapt resource value: 0x7f04000f
+			public const int fadeout = 2130968591;
+			
+			// aapt resource value: 0x7f040010
+			public const int scale_in = 2130968592;
+			
+			// aapt resource value: 0x7f040011
+			public const int scale_out = 2130968593;
+			
 			static Animation()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
@@ -3325,8 +3337,8 @@ namespace Project4Bicycle.Droid
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f0b0074
-			public const int action0 = 2131427444;
+			// aapt resource value: 0x7f0b0083
+			public const int action0 = 2131427459;
 			
 			// aapt resource value: 0x7f0b0055
 			public const int action_bar = 2131427413;
@@ -3352,8 +3364,8 @@ namespace Project4Bicycle.Droid
 			// aapt resource value: 0x7f0b0056
 			public const int action_context_bar = 2131427414;
 			
-			// aapt resource value: 0x7f0b0078
-			public const int action_divider = 2131427448;
+			// aapt resource value: 0x7f0b0087
+			public const int action_divider = 2131427463;
 			
 			// aapt resource value: 0x7f0b0002
 			public const int action_menu_divider = 2131427330;
@@ -3379,8 +3391,11 @@ namespace Project4Bicycle.Droid
 			// aapt resource value: 0x7f0b001c
 			public const int always = 2131427356;
 			
+			// aapt resource value: 0x7f0b007b
+			public const int art = 2131427451;
+			
 			// aapt resource value: 0x7f0b006c
-			public const int art = 2131427436;
+			public const int axisLayout = 2131427436;
 			
 			// aapt resource value: 0x7f0b0019
 			public const int beginning = 2131427353;
@@ -3388,14 +3403,17 @@ namespace Project4Bicycle.Droid
 			// aapt resource value: 0x7f0b0028
 			public const int bottom = 2131427368;
 			
+			// aapt resource value: 0x7f0b0091
+			public const int bottom_labels = 2131427473;
+			
 			// aapt resource value: 0x7f0b004b
 			public const int buttonPanel = 2131427403;
 			
-			// aapt resource value: 0x7f0b0071
-			public const int buttons = 2131427441;
+			// aapt resource value: 0x7f0b0080
+			public const int buttons = 2131427456;
 			
-			// aapt resource value: 0x7f0b0075
-			public const int cancel_action = 2131427445;
+			// aapt resource value: 0x7f0b0084
+			public const int cancel_action = 2131427460;
 			
 			// aapt resource value: 0x7f0b0029
 			public const int center = 2131427369;
@@ -3406,11 +3424,17 @@ namespace Project4Bicycle.Droid
 			// aapt resource value: 0x7f0b002b
 			public const int center_vertical = 2131427371;
 			
+			// aapt resource value: 0x7f0b0066
+			public const int chartAreaLayout = 2131427430;
+			
+			// aapt resource value: 0x7f0b0065
+			public const int chartTitle = 2131427429;
+			
 			// aapt resource value: 0x7f0b004d
 			public const int checkbox = 2131427405;
 			
-			// aapt resource value: 0x7f0b007b
-			public const int chronometer = 2131427451;
+			// aapt resource value: 0x7f0b008a
+			public const int chronometer = 2131427466;
 			
 			// aapt resource value: 0x7f0b0031
 			public const int clip_horizontal = 2131427377;
@@ -3421,6 +3445,9 @@ namespace Project4Bicycle.Droid
 			// aapt resource value: 0x7f0b001d
 			public const int collapseActionView = 2131427357;
 			
+			// aapt resource value: 0x7f0b0092
+			public const int content = 2131427474;
+			
 			// aapt resource value: 0x7f0b0046
 			public const int contentPanel = 2131427398;
 			
@@ -3430,20 +3457,23 @@ namespace Project4Bicycle.Droid
 			// aapt resource value: 0x7f0b0049
 			public const int customPanel = 2131427401;
 			
+			// aapt resource value: 0x7f0b006a
+			public const int dataMarkerLayout = 2131427434;
+			
 			// aapt resource value: 0x7f0b0053
 			public const int decor_content_parent = 2131427411;
 			
 			// aapt resource value: 0x7f0b003e
 			public const int default_activity_button = 2131427390;
 			
-			// aapt resource value: 0x7f0b006b
-			public const int default_control_frame = 2131427435;
+			// aapt resource value: 0x7f0b007a
+			public const int default_control_frame = 2131427450;
 			
 			// aapt resource value: 0x7f0b000d
 			public const int disableHome = 2131427341;
 			
-			// aapt resource value: 0x7f0b0072
-			public const int disconnect = 2131427442;
+			// aapt resource value: 0x7f0b0081
+			public const int disconnect = 2131427457;
 			
 			// aapt resource value: 0x7f0b0057
 			public const int edit_query = 2131427415;
@@ -3451,8 +3481,8 @@ namespace Project4Bicycle.Droid
 			// aapt resource value: 0x7f0b001a
 			public const int end = 2131427354;
 			
-			// aapt resource value: 0x7f0b0080
-			public const int end_padder = 2131427456;
+			// aapt resource value: 0x7f0b008f
+			public const int end_padder = 2131427471;
 			
 			// aapt resource value: 0x7f0b0022
 			public const int enterAlways = 2131427362;
@@ -3481,6 +3511,12 @@ namespace Project4Bicycle.Droid
 			// aapt resource value: 0x7f0b0036
 			public const int @fixed = 2131427382;
 			
+			// aapt resource value: 0x7f0b0067
+			public const int gridLineLayout = 2131427431;
+			
+			// aapt resource value: 0x7f0b0093
+			public const int gridLinesLayout = 2131427475;
+			
 			// aapt resource value: 0x7f0b0004
 			public const int home = 2131427332;
 			
@@ -3490,23 +3526,41 @@ namespace Project4Bicycle.Droid
 			// aapt resource value: 0x7f0b0040
 			public const int icon = 2131427392;
 			
+			// aapt resource value: 0x7f0b0071
+			public const int iconView = 2131427441;
+			
 			// aapt resource value: 0x7f0b001e
 			public const int ifRoom = 2131427358;
 			
 			// aapt resource value: 0x7f0b003d
 			public const int image = 2131427389;
 			
-			// aapt resource value: 0x7f0b007f
-			public const int info = 2131427455;
+			// aapt resource value: 0x7f0b008e
+			public const int info = 2131427470;
+			
+			// aapt resource value: 0x7f0b0072
+			public const int label = 2131427442;
 			
 			// aapt resource value: 0x7f0b002d
 			public const int left = 2131427373;
 			
-			// aapt resource value: 0x7f0b0079
-			public const int line1 = 2131427449;
+			// aapt resource value: 0x7f0b006e
+			public const int legendHeader = 2131427438;
 			
-			// aapt resource value: 0x7f0b007d
-			public const int line3 = 2131427453;
+			// aapt resource value: 0x7f0b0070
+			public const int legendItemsLayout = 2131427440;
+			
+			// aapt resource value: 0x7f0b006f
+			public const int legendScrollView = 2131427439;
+			
+			// aapt resource value: 0x7f0b006d
+			public const int legendViewGroup = 2131427437;
+			
+			// aapt resource value: 0x7f0b0088
+			public const int line1 = 2131427464;
+			
+			// aapt resource value: 0x7f0b008c
+			public const int line3 = 2131427468;
 			
 			// aapt resource value: 0x7f0b000a
 			public const int listMode = 2131427338;
@@ -3514,20 +3568,20 @@ namespace Project4Bicycle.Droid
 			// aapt resource value: 0x7f0b003f
 			public const int list_item = 2131427391;
 			
-			// aapt resource value: 0x7f0b0077
-			public const int media_actions = 2131427447;
+			// aapt resource value: 0x7f0b0086
+			public const int media_actions = 2131427462;
 			
-			// aapt resource value: 0x7f0b006a
-			public const int media_route_control_frame = 2131427434;
+			// aapt resource value: 0x7f0b0079
+			public const int media_route_control_frame = 2131427449;
 			
-			// aapt resource value: 0x7f0b0066
-			public const int media_route_list = 2131427430;
+			// aapt resource value: 0x7f0b0075
+			public const int media_route_list = 2131427445;
 			
-			// aapt resource value: 0x7f0b006f
-			public const int media_route_volume_layout = 2131427439;
+			// aapt resource value: 0x7f0b007e
+			public const int media_route_volume_layout = 2131427454;
 			
-			// aapt resource value: 0x7f0b0070
-			public const int media_route_volume_slider = 2131427440;
+			// aapt resource value: 0x7f0b007f
+			public const int media_route_volume_slider = 2131427455;
 			
 			// aapt resource value: 0x7f0b001b
 			public const int middle = 2131427355;
@@ -3556,8 +3610,8 @@ namespace Project4Bicycle.Droid
 			// aapt resource value: 0x7f0b0027
 			public const int pin = 2131427367;
 			
-			// aapt resource value: 0x7f0b006d
-			public const int play_pause = 2131427437;
+			// aapt resource value: 0x7f0b007c
+			public const int play_pause = 2131427452;
 			
 			// aapt resource value: 0x7f0b0005
 			public const int progress_circular = 2131427333;
@@ -3571,8 +3625,11 @@ namespace Project4Bicycle.Droid
 			// aapt resource value: 0x7f0b002e
 			public const int right = 2131427374;
 			
-			// aapt resource value: 0x7f0b0068
-			public const int route_name = 2131427432;
+			// aapt resource value: 0x7f0b0064
+			public const int rootLayout = 2131427428;
+			
+			// aapt resource value: 0x7f0b0077
+			public const int route_name = 2131427447;
 			
 			// aapt resource value: 0x7f0b0015
 			public const int screen = 2131427349;
@@ -3620,7 +3677,10 @@ namespace Project4Bicycle.Droid
 			public const int select_dialog_listview = 2131427427;
 			
 			// aapt resource value: 0x7f0b0069
-			public const int settings = 2131427433;
+			public const int seriesLayout = 2131427433;
+			
+			// aapt resource value: 0x7f0b0078
+			public const int settings = 2131427448;
 			
 			// aapt resource value: 0x7f0b004e
 			public const int shortcut = 2131427406;
@@ -3634,11 +3694,11 @@ namespace Project4Bicycle.Droid
 			// aapt resource value: 0x7f0b0012
 			public const int showTitle = 2131427346;
 			
-			// aapt resource value: 0x7f0b0065
-			public const int snackbar_action = 2131427429;
+			// aapt resource value: 0x7f0b0074
+			public const int snackbar_action = 2131427444;
 			
-			// aapt resource value: 0x7f0b0064
-			public const int snackbar_text = 2131427428;
+			// aapt resource value: 0x7f0b0073
+			public const int snackbar_text = 2131427443;
 			
 			// aapt resource value: 0x7f0b0007
 			public const int split_action_bar = 2131427335;
@@ -3655,38 +3715,47 @@ namespace Project4Bicycle.Droid
 			// aapt resource value: 0x7f0b002f
 			public const int start = 2131427375;
 			
-			// aapt resource value: 0x7f0b0076
-			public const int status_bar_latest_event_content = 2131427446;
+			// aapt resource value: 0x7f0b0085
+			public const int status_bar_latest_event_content = 2131427461;
 			
-			// aapt resource value: 0x7f0b0073
-			public const int stop = 2131427443;
+			// aapt resource value: 0x7f0b0082
+			public const int stop = 2131427458;
+			
+			// aapt resource value: 0x7f0b0068
+			public const int stripLinesLayout = 2131427432;
 			
 			// aapt resource value: 0x7f0b0060
 			public const int submit_area = 2131427424;
 			
-			// aapt resource value: 0x7f0b006e
-			public const int subtitle = 2131427438;
+			// aapt resource value: 0x7f0b007d
+			public const int subtitle = 2131427453;
 			
 			// aapt resource value: 0x7f0b000c
 			public const int tabMode = 2131427340;
 			
-			// aapt resource value: 0x7f0b007e
-			public const int text = 2131427454;
+			// aapt resource value: 0x7f0b006b
+			public const int technicalIndicatorLayout = 2131427435;
 			
-			// aapt resource value: 0x7f0b007c
-			public const int text2 = 2131427452;
+			// aapt resource value: 0x7f0b008d
+			public const int text = 2131427469;
+			
+			// aapt resource value: 0x7f0b008b
+			public const int text2 = 2131427467;
 			
 			// aapt resource value: 0x7f0b0048
 			public const int textSpacerNoButtons = 2131427400;
 			
-			// aapt resource value: 0x7f0b007a
-			public const int time = 2131427450;
+			// aapt resource value: 0x7f0b0094
+			public const int thumbLayout = 2131427476;
+			
+			// aapt resource value: 0x7f0b0089
+			public const int time = 2131427465;
 			
 			// aapt resource value: 0x7f0b0041
 			public const int title = 2131427393;
 			
-			// aapt resource value: 0x7f0b0067
-			public const int title_bar = 2131427431;
+			// aapt resource value: 0x7f0b0076
+			public const int title_bar = 2131427446;
 			
 			// aapt resource value: 0x7f0b0044
 			public const int title_template = 2131427396;
@@ -3696,6 +3765,9 @@ namespace Project4Bicycle.Droid
 			
 			// aapt resource value: 0x7f0b0043
 			public const int topPanel = 2131427395;
+			
+			// aapt resource value: 0x7f0b0090
+			public const int top_labels = 2131427472;
 			
 			// aapt resource value: 0x7f0b0008
 			public const int up = 2131427336;
@@ -3829,76 +3901,88 @@ namespace Project4Bicycle.Droid
 			public const int abc_select_dialog_material = 2130903063;
 			
 			// aapt resource value: 0x7f030018
-			public const int design_layout_snackbar = 2130903064;
+			public const int chart_layout = 2130903064;
 			
 			// aapt resource value: 0x7f030019
-			public const int design_layout_snackbar_include = 2130903065;
+			public const int chart_legend = 2130903065;
 			
 			// aapt resource value: 0x7f03001a
-			public const int design_layout_tab_icon = 2130903066;
+			public const int chart_legend_item = 2130903066;
 			
 			// aapt resource value: 0x7f03001b
-			public const int design_layout_tab_text = 2130903067;
+			public const int design_layout_snackbar = 2130903067;
 			
 			// aapt resource value: 0x7f03001c
-			public const int design_navigation_item = 2130903068;
+			public const int design_layout_snackbar_include = 2130903068;
 			
 			// aapt resource value: 0x7f03001d
-			public const int design_navigation_item_header = 2130903069;
+			public const int design_layout_tab_icon = 2130903069;
 			
 			// aapt resource value: 0x7f03001e
-			public const int design_navigation_item_separator = 2130903070;
+			public const int design_layout_tab_text = 2130903070;
 			
 			// aapt resource value: 0x7f03001f
-			public const int design_navigation_item_subheader = 2130903071;
+			public const int design_navigation_item = 2130903071;
 			
 			// aapt resource value: 0x7f030020
-			public const int design_navigation_menu = 2130903072;
+			public const int design_navigation_item_header = 2130903072;
 			
 			// aapt resource value: 0x7f030021
-			public const int mr_media_route_chooser_dialog = 2130903073;
+			public const int design_navigation_item_separator = 2130903073;
 			
 			// aapt resource value: 0x7f030022
-			public const int mr_media_route_controller_material_dialog_b = 2130903074;
+			public const int design_navigation_item_subheader = 2130903074;
 			
 			// aapt resource value: 0x7f030023
-			public const int mr_media_route_list_item = 2130903075;
+			public const int design_navigation_menu = 2130903075;
 			
 			// aapt resource value: 0x7f030024
-			public const int notification_media_action = 2130903076;
+			public const int mr_media_route_chooser_dialog = 2130903076;
 			
 			// aapt resource value: 0x7f030025
-			public const int notification_media_cancel_action = 2130903077;
+			public const int mr_media_route_controller_material_dialog_b = 2130903077;
 			
 			// aapt resource value: 0x7f030026
-			public const int notification_template_big_media = 2130903078;
+			public const int mr_media_route_list_item = 2130903078;
 			
 			// aapt resource value: 0x7f030027
-			public const int notification_template_big_media_narrow = 2130903079;
+			public const int notification_media_action = 2130903079;
 			
 			// aapt resource value: 0x7f030028
-			public const int notification_template_lines = 2130903080;
+			public const int notification_media_cancel_action = 2130903080;
 			
 			// aapt resource value: 0x7f030029
-			public const int notification_template_media = 2130903081;
+			public const int notification_template_big_media = 2130903081;
 			
 			// aapt resource value: 0x7f03002a
-			public const int notification_template_part_chronometer = 2130903082;
+			public const int notification_template_big_media_narrow = 2130903082;
 			
 			// aapt resource value: 0x7f03002b
-			public const int notification_template_part_time = 2130903083;
+			public const int notification_template_lines = 2130903083;
 			
 			// aapt resource value: 0x7f03002c
-			public const int select_dialog_item_material = 2130903084;
+			public const int notification_template_media = 2130903084;
 			
 			// aapt resource value: 0x7f03002d
-			public const int select_dialog_multichoice_material = 2130903085;
+			public const int notification_template_part_chronometer = 2130903085;
 			
 			// aapt resource value: 0x7f03002e
-			public const int select_dialog_singlechoice_material = 2130903086;
+			public const int notification_template_part_time = 2130903086;
 			
 			// aapt resource value: 0x7f03002f
-			public const int support_simple_spinner_dropdown_item = 2130903087;
+			public const int range_navigator_layout = 2130903087;
+			
+			// aapt resource value: 0x7f030030
+			public const int select_dialog_item_material = 2130903088;
+			
+			// aapt resource value: 0x7f030031
+			public const int select_dialog_multichoice_material = 2130903089;
+			
+			// aapt resource value: 0x7f030032
+			public const int select_dialog_singlechoice_material = 2130903090;
+			
+			// aapt resource value: 0x7f030033
+			public const int support_simple_spinner_dropdown_item = 2130903091;
 			
 			static Layout()
 			{
@@ -4006,8 +4090,20 @@ namespace Project4Bicycle.Droid
 			// aapt resource value: 0x7f05000a
 			public const int mr_user_route_category_name = 2131034122;
 			
+			// aapt resource value: 0x7f050020
+			public const int q = 2131034144;
+			
+			// aapt resource value: 0x7f050021
+			public const int quarter = 2131034145;
+			
 			// aapt resource value: 0x7f05001c
 			public const int status_bar_notification_info_overflow = 2131034140;
+			
+			// aapt resource value: 0x7f050022
+			public const int w = 2131034146;
+			
+			// aapt resource value: 0x7f050023
+			public const int week = 2131034147;
 			
 			static String()
 			{
