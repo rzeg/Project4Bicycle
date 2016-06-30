2ef9c69740bf1a0c099f3e2bff35e52527310b9f
Rick Zegelaar <Rick Zegelaar>
2016-06-30 15:25:45 +0200

    Remove some crap code.

--- Project4Bicycle/Project4Bicycle/Pages/ContainerOverviewPage.cs
+++ Project4Bicycle/Project4Bicycle/Pages/ContainerOverviewPage.cs
@@ -23,46 +23,12 @@ namespace Project4Bicycle
             chart.PrimaryAxis = horizontalAxis;
             chart.SecondaryAxis = verticalAxis;
 
-            //BikeContainer bk = new BikeContainer();
-            //bk.Description = "Lel";
-            //bk.ID = "1";
-            //bk.Neighbourhood = "Hookah";
-            //BikeContainer bk3 = new BikeContainer();
-            //bk3.Description = "Lel";
-            //bk3.ID = "2";
-            //bk3.Neighbourhood = "Hookah";
-            //BikeContainer bk2 = new BikeContainer();
-            //bk2.Description = "Lel";
-            //bk2.ID = "2";
-            //bk2.Neighbourhood = "Hook";
-            //BikeGraphModel bg = new BikeGraphModel();
-            //Neighbourhood nb = new Neighbourhood();
-            //nb.Name = bk.Neighbourhood;
-            //nb.AddContainer(bk);
-            //nb.AddContainer(bk3);
-            //Neighbourhood nb2 = new Neighbourhood();
-            //nb2.Name = bk2.Neighbourhood;
-            //nb2.AddContainer(bk2);
-            //Neighbourhood nb3 = new Neighbourhood();
-
-            //List<Neighbourhood> Neighbourhoods = new List<Neighbourhood>();
-            //Neighbourhoods.Add(nb);
-            //Neighbourhoods.Add(nb2);
-            //bg.AddData(nb);
-            //bg.AddData(nb2);
-            GetShit();
-            //foreach (Neighbourhood nb in gen.Neighbourhoods)
-            //{
-            //    Debug.WriteLine(nb.Name);
-            //    bg.AddData(nb);
-            //}
-            //var t = gen.GenerateNeighbourhoods();
-            //var result = t.Result;
+            GenerateGraph();
      
             this.Content = chart;
         }
 
-        public async void GetShit()
+        public async void GenerateGraph()
         {
             Top5Generator gen = new Top5Generator();
             BikeGraphModel bg = await gen.GenerateNeighbourhoods();
